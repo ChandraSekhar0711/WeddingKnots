a0173547cadf27f6b32e7935764a7b4c9376c90c
cthogiri <cthogiri@apple.com>
2023-06-06 18:16:01 +0530

    light changes

--- src/Components/CartDetails/CartDetails.jsx
+++ src/Components/CartDetails/CartDetails.jsx
@@ -13,7 +13,8 @@ export function CartDetails(){
     const navigte= useNavigate();
     const dispatch = useDispatch();
     function orderDelete(order){
-        if(window.confirm(`Do You really want to delete the order?`)){
+        if(window.confirm(`Do You really want to delete the order ${order.id}`)){
+            
             try{
                 OrderAPI.deleteById(order.id);
                 dispatch(deleteOrder(order));
