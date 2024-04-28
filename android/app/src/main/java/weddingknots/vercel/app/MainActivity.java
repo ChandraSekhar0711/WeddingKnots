03d3d4f936c57d95d3ba22c75aecfc5cea248b5d
chandutogiri3 <chandutogiri3@gmail.com>
2024-04-28 08:29:59 +0000

    native added

---
+++ android/app/src/main/java/weddingknots/vercel/app/MainActivity.java
@@ -0,0 +1,5 @@
+package weddingknots.vercel.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'weddingknots.vercel.app',
+  appName: 'WeddingKnots',
+  webDir: 'build'
+};
+
+export default config;
